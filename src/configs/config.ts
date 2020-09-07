fda012ade93a9e53a0613c757a9b5e6202ea631e
camiloJTG <camilojtg.dev@gmail.com>
2020-09-07 00:33:47 -0300

    feat: Add new config to jwt

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,6 +1,9 @@
 export const config = {
   environment: {
-    port: process.env.PORT,
+    port: process.env.PORT || 3000,
     dev: process.env.NODE_ENV !== 'production',
+  },
+  jwt: {
+    secretKey: process.env.JWT_SECRET || '',
   },
 };

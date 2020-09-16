2a79fb356c947f0433e2474b2ffb1718c1b2ee0b
camiloJTG <camilojtg.dev@gmail.com>
2020-09-16 16:06:45 -0300

    feat: Add new middlewares for file administration and new logic to validator handler

--- src/controllers/middlewares/validatorHandler.ts
+++ src/controllers/middlewares/validatorHandler.ts
@@ -1,16 +1,29 @@
 import { validate } from '../utils/joi';
 import { NextFunction, Request, Response } from 'express';
 import { SchemaMap } from '@hapi/joi';
+import { unlink } from 'fs-extra';
 
 export const validatorHandler = (schema: SchemaMap, check: string) => {
   return (req: Request, _res: Response, next: NextFunction): void => {
     if (check === 'body') {
       const error = validate(req.body, schema);
-      error ? next(error) : next();
+      if (error) {
+        if (req.file) {
+          unlink(req.file.path);
+        }
+        next(error);
+      }
+      next();
     }
     if (check === 'params') {
       const error = validate(req.params, schema);
-      error ? next(error) : next();
+      if (error) {
+        if (req.file) {
+          unlink(req.file.path);
+        }
+        next(error);
+      }
+      next();
     }
   };
 };

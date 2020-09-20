647f16d82345826f18f6aef72c8f04f9c082536c
camiloJTG <camilojtg.dev@gmail.com>
2020-09-20 16:44:08 -0300

    fix: Review access logic

--- src/services/access.service.ts
+++ src/services/access.service.ts
@@ -1,11 +1,22 @@
 import { getRepository } from 'typeorm';
 import { sign, ILogin } from '../controllers/utils/jwt';
 import { Users } from '../models/user.model';
+import { compare } from '../controllers/utils/bcrypt';
 
 export const accessLogin = async (login: ILogin): Promise<string> => {
   const result = await getRepository(Users).findOne({
-    where: { MAIL: login.MAIL, PASSWORD: login.PASSWORD },
+    where: { MAIL: login.MAIL },
   });
+  if (result === undefined) {
+    throw new Error(`Invalid credentials`);
+  }
+
+  const comparePassword = await compare(login.PASSWORD, result?.PASSWORD);
+
+  if (!comparePassword) {
+    throw new Error(`Invalid credentials`);
+  }
+
   const generateToken = sign({
     MAIL: result?.MAIL,
     ROLE: result?.ROLE,
